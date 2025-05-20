e46563f838930a140491fd934d21f9001da2be67
Kamil Dybicz <kamil.dybicz@gmail.com>
2025-05-20 18:21:08 +0200

    updating lambda for processing incomming emails

--- service/email-processor.ts
+++ service/email-processor.ts
@@ -3,17 +3,20 @@ import 'source-map-support/register';
 import type { SESHandler } from 'aws-lambda';
 
 import { IncomingEmailProcessor } from './processor/IncomingEmailProcessor';
+import { EmailDatabase } from './tools/EmailDatabase';
 import { EmailParser } from './tools/EmailParser';
 import { S3FileSystem } from './tools/S3FileSystem';
 import log from './tools/log';
 
-const fileSystem = new S3FileSystem();
+const emailDatabase = new EmailDatabase();
 const emailParser = new EmailParser();
+const fileSystem = new S3FileSystem();
 
 const emailProcessor = new IncomingEmailProcessor(
   fileSystem,
   process.env.EMAIL_BUCKET_NAME ?? '',
   emailParser,
+  emailDatabase,
 );
 
 export const handler: SESHandler = async (event) => {
--- test/service/processor/IncomingEmailHandler.test.ts
+++ test/service/processor/IncomingEmailHandler.test.ts
@@ -3,6 +3,7 @@ import * as fs from 'fs';
 import { IncomingEmailProcessor } from '../../../service/processor/IncomingEmailProcessor';
 import { EmailParser } from '../../../service/tools/EmailParser';
 import { S3FileSystem } from '../../../service/tools/S3FileSystem';
+import { EmailDatabase } from '../../../service/tools/EmailDatabase';
 
 describe('EmailParser tests', () => {
   const EMAIL_BUCKET_NAME = 'test-bucket';
@@ -10,13 +11,15 @@ describe('EmailParser tests', () => {
 
   const emailParser = new EmailParser();
 
+  let emailDatabase: EmailDatabase;
   let fileSystem: S3FileSystem;
 
   beforeEach(() => {
+    emailDatabase = {
+      store: jest.fn(),
+    } as unknown as EmailDatabase;
     fileSystem = {
       getObject: jest.fn(),
-      copyObject: jest.fn(),
-      deleteObject: jest.fn(),
     } as unknown as S3FileSystem;
   });
 
@@ -30,15 +33,13 @@ describe('EmailParser tests', () => {
       },
     });
     // and:
-    const processor = new IncomingEmailProcessor(fileSystem, EMAIL_BUCKET_NAME, emailParser);
+    const processor = new IncomingEmailProcessor(fileSystem, EMAIL_BUCKET_NAME, emailParser, emailDatabase);
 
     // when:
     await processor.processEmail(MESSAGE_ID);
     // then:
-    expect(fileSystem.copyObject).toHaveBeenCalledTimes(1);
-    expect(fileSystem.copyObject).toHaveBeenCalledWith(EMAIL_BUCKET_NAME, MESSAGE_ID, 'janedoe/8516099335999');
-    // and:
-    expect(fileSystem.deleteObject).toHaveBeenCalledTimes(1);
+    expect(emailDatabase.store).toHaveBeenCalledTimes(1);
+    expect(emailDatabase.store).toHaveBeenCalledWith("message-id", "janedoe", "john.doe@example.com", "test", new Date('2017-01-08T18:37:44.000Z'));
   });
 
   test('Handle complex email', async () => {
@@ -51,16 +52,14 @@ describe('EmailParser tests', () => {
       },
     });
     // and:
-    const processor = new IncomingEmailProcessor(fileSystem, EMAIL_BUCKET_NAME, emailParser);
+    const processor = new IncomingEmailProcessor(fileSystem, EMAIL_BUCKET_NAME, emailParser, emailDatabase);
 
     // when:
     await processor.processEmail(MESSAGE_ID);
     // then:
-    expect(fileSystem.copyObject).toHaveBeenCalledTimes(2);
-    expect(fileSystem.copyObject).toHaveBeenCalledWith(EMAIL_BUCKET_NAME, MESSAGE_ID, 'johndoe/8523641211999');
-    expect(fileSystem.copyObject).toHaveBeenCalledWith(EMAIL_BUCKET_NAME, MESSAGE_ID, 'janedoe/8523641211999');
-    // and:
-    expect(fileSystem.deleteObject).toHaveBeenCalledTimes(1);
+    expect(emailDatabase.store).toHaveBeenCalledTimes(2);
+    expect(emailDatabase.store).toHaveBeenCalledWith("message-id", "johndoe", "john.doe@example.com", "Nodemailer is unicode friendly ✔ (1476358788189)", new Date('2016-10-13T11:39:48.000Z'));
+    expect(emailDatabase.store).toHaveBeenCalledWith("message-id", "janedoe", "john.doe@example.com", "Nodemailer is unicode friendly ✔ (1476358788189)", new Date('2016-10-13T11:39:48.000Z'));
   });
 
   test('Handle cc email', async () => {
@@ -73,16 +72,14 @@ describe('EmailParser tests', () => {
       },
     });
     // and:
-    const processor = new IncomingEmailProcessor(fileSystem, EMAIL_BUCKET_NAME, emailParser);
+    const processor = new IncomingEmailProcessor(fileSystem, EMAIL_BUCKET_NAME, emailParser, emailDatabase);
 
     // when:
     await processor.processEmail(MESSAGE_ID);
     // then:
-    expect(fileSystem.copyObject).toHaveBeenCalledTimes(2);
-    expect(fileSystem.copyObject).toHaveBeenCalledWith(EMAIL_BUCKET_NAME, MESSAGE_ID, 'janedoe/8355574288999');
-    expect(fileSystem.copyObject).toHaveBeenCalledWith(EMAIL_BUCKET_NAME, MESSAGE_ID, 'mariadoe/8355574288999');
-    // and:
-    expect(fileSystem.deleteObject).toHaveBeenCalledTimes(1);
+    expect(emailDatabase.store).toHaveBeenCalledTimes(2);
+    expect(emailDatabase.store).toHaveBeenCalledWith("message-id", "janedoe", "john.doe@example.com", "hidden", new Date('2022-02-09T16:55:11.000Z'));
+    expect(emailDatabase.store).toHaveBeenCalledWith("message-id", "mariadoe", "john.doe@example.com", "hidden", new Date('2022-02-09T16:55:11.000Z'));
   });
 
   test('Handle bcc email', async () => {
@@ -95,14 +92,12 @@ describe('EmailParser tests', () => {
       },
     });
     // and:
-    const processor = new IncomingEmailProcessor(fileSystem, EMAIL_BUCKET_NAME, emailParser);
+    const processor = new IncomingEmailProcessor(fileSystem, EMAIL_BUCKET_NAME, emailParser, emailDatabase);
 
     // when:
     await processor.processEmail(MESSAGE_ID);
     // then:
-    expect(fileSystem.copyObject).toHaveBeenCalledTimes(1);
-    expect(fileSystem.copyObject).toHaveBeenCalledWith(EMAIL_BUCKET_NAME, MESSAGE_ID, 'hidden/8355574288999');
-    // and:
-    expect(fileSystem.deleteObject).toHaveBeenCalledTimes(1);
+    expect(emailDatabase.store).toHaveBeenCalledTimes(1);
+    expect(emailDatabase.store).toHaveBeenCalledWith("message-id", "hidden", "john.doe@example.com", "hidden", new Date('2022-02-09T16:55:11.000Z'));
   });
 });
