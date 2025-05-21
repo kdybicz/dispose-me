0c353c045e60c708ddaa38da636092e2f6a792ba
Kamil Dybicz <kamil.dybicz@gmail.com>
2025-05-21 10:35:54 +0200

    Update log.ts

--- service/tools/log.ts
+++ service/tools/log.ts
@@ -1,36 +1,31 @@
 export default class Logger {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  static trace(message?: any, ...optionalParams: any[]): void {
+  static trace(message: string, ...optionalParams: unknown[]): void {
     if (Logger.logForLevel(5)) {
-      console.trace(message, optionalParams);
+      console.trace(message, ...optionalParams);
     }
   }
 
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  static debug(message?: any, ...optionalParams: any[]): void {
+  static debug(message: string, ...optionalParams: unknown[]): void {
     if (Logger.logForLevel(4)) {
-      console.debug(message, optionalParams);
+      console.debug(message, ...optionalParams);
     }
   }
 
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  static info(message?: any, ...optionalParams: any[]): void {
+  static info(message: string, ...optionalParams: unknown[]): void {
     if (Logger.logForLevel(3)) {
-      console.info(message, optionalParams);
+      console.info(message, ...optionalParams);
     }
   }
 
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  static warn(message?: any, ...optionalParams: any[]): void {
+  static warn(message: string, ...optionalParams: unknown[]): void {
     if (Logger.logForLevel(2)) {
-      console.warn(message, optionalParams);
+      console.warn(message, ...optionalParams);
     }
   }
 
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  static error(message?: any, ...optionalParams: any[]): void {
+  static error(message: string, ...optionalParams: unknown[]): void {
     if (Logger.logForLevel(1)) {
-      console.error(message, optionalParams);
+      console.error(message, ...optionalParams);
     }
   }
 
